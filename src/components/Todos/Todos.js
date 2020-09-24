386ef7cb8e1a60bf3b73e450863b70cc76ac788a
Magdalena <magmarnowak@gmail.com>
2020-09-24 11:39:21 +0200

    Display error below form

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,24 +1,25 @@
-import React, { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { fetchData } from "../../common/utils"
-import { actions } from "../../store/todo"
-import { TodoForm } from "./TodoForm"
-import { Form } from "../../common/hooks"
-import { initialState, onSubmit, validate } from "./TodoForm/utils"
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../../common/utils";
+import { actions } from "../../store/todo";
+import { TodoForm } from "./TodoForm";
+import { Form } from "../../common/hooks";
+import { initialState, onSubmit, validate } from "./TodoForm/utils";
 
 export const Todos = () => {
   const dispatch = useDispatch();
-  const todos = useSelector(state => state.todos);
+  const todos = useSelector((state) => state.todos);
+  const error = useSelector((state) => state.error);
   useEffect(() => {
     fetchData("todos")
-      .then(todos => {
-        dispatch(actions.updateTodos.create(todos))
+      .then((todos) => {
+        dispatch(actions.updateTodos.create(todos));
       })
       .catch(({ message }) => {
-        dispatch(actions.setError.create(message))
-      })
-  }, [dispatch])
+        dispatch(actions.setError.create(message));
+      });
+  }, [dispatch]);
+
   return (
     <div>
       {todos.map(({ id, title }) => (
@@ -27,6 +28,7 @@ export const Todos = () => {
       <Form {...{ initialState, onSubmit, validate }}>
         <TodoForm />
       </Form>
+      <div style={{ color: "red" }}>{error}</div>
     </div>
-  )
-}
+  );
+};
