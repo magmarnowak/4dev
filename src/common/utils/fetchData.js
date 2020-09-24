f6c08e08864f6b3856ed8fefa165a0ce24f402f5
Magdalena <magmarnowak@gmail.com>
2020-09-24 15:52:56 +0200

    Post todos on submit

--- src/common/utils/fetchData.js
+++ src/common/utils/fetchData.js
@@ -1,15 +1,27 @@
-import { baseUrl } from "."
+import { baseUrl } from ".";
 
-const handleError = res => {
+const handleError = (res) => {
   if (!res.ok) {
-    throw new Error(res.statusText)
+    throw new Error(res.statusText);
   }
-  return res
-}
+  return res;
+};
 
-export const delay = time => new Promise(resolve => setTimeout(resolve, time))
+export const delay = (time) =>
+  new Promise((resolve) => setTimeout(resolve, time));
 
-export const fetchData = endpoint =>
+export const fetchData = (endpoint) =>
   fetch(baseUrl(endpoint))
     .then(handleError)
-    .then(res => res.json())
+    .then((res) => res.json());
+
+export const postData = (endpoint, values) =>
+  fetch(baseUrl(endpoint), {
+    method: "POST",
+    body: JSON.stringify(values),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+    .then(handleError)
+    .then((res) => res.json());
--- src/components/Todos/TodoForm/utils.js
+++ src/components/Todos/TodoForm/utils.js
@@ -1,14 +1,20 @@
+import { postData } from "../../../common/utils/fetchData";
+
 export const initialState = {
   values: {
     title: "",
     description: "",
   },
   errors: {},
-}
-export const onSubmit = values => {
-  console.log(values)
-}
-export const validate = values => {
-  const required = ["title"]
-  return required.reduce((acc, curr) => (values[curr] ? acc : { ...acc, [curr]: "require" }), {})
-}
+};
+export const onSubmit = (values) => {
+  postData("todos", values);
+};
+export const validate = (values) => {
+  const required = ["title"];
+  return required.reduce(
+    (acc, curr) =>
+      values[curr] ? acc : { ...acc, [curr]: "require" },
+    {}
+  );
+};
